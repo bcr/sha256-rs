318f3c5c602d7e4bddec287b7a64e86502b91c3c
Blake Ramsdell <blaker@gmail.com>
2022-11-26 11:38:49 -0800

    rustfmt formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,74 @@
 #![allow(non_snake_case)]
 
-use std::num::Wrapping;
 use std::mem;
+use std::num::Wrapping;
 
-static K: [Wrapping<u32>; 64] = [ Wrapping(0x428a2f98), Wrapping(0x71374491), Wrapping(0xb5c0fbcf), Wrapping(0xe9b5dba5),
-                        Wrapping(0x3956c25b), Wrapping(0x59f111f1), Wrapping(0x923f82a4), Wrapping(0xab1c5ed5),
-                        Wrapping(0xd807aa98), Wrapping(0x12835b01), Wrapping(0x243185be), Wrapping(0x550c7dc3),
-                        Wrapping(0x72be5d74), Wrapping(0x80deb1fe), Wrapping(0x9bdc06a7), Wrapping(0xc19bf174),
-                        Wrapping(0xe49b69c1), Wrapping(0xefbe4786), Wrapping(0x0fc19dc6), Wrapping(0x240ca1cc),
-                        Wrapping(0x2de92c6f), Wrapping(0x4a7484aa), Wrapping(0x5cb0a9dc), Wrapping(0x76f988da),
-                        Wrapping(0x983e5152), Wrapping(0xa831c66d), Wrapping(0xb00327c8), Wrapping(0xbf597fc7),
-                        Wrapping(0xc6e00bf3), Wrapping(0xd5a79147), Wrapping(0x06ca6351), Wrapping(0x14292967),
-                        Wrapping(0x27b70a85), Wrapping(0x2e1b2138), Wrapping(0x4d2c6dfc), Wrapping(0x53380d13),
-                        Wrapping(0x650a7354), Wrapping(0x766a0abb), Wrapping(0x81c2c92e), Wrapping(0x92722c85),
-                        Wrapping(0xa2bfe8a1), Wrapping(0xa81a664b), Wrapping(0xc24b8b70), Wrapping(0xc76c51a3),
-                        Wrapping(0xd192e819), Wrapping(0xd6990624), Wrapping(0xf40e3585), Wrapping(0x106aa070),
-                        Wrapping(0x19a4c116), Wrapping(0x1e376c08), Wrapping(0x2748774c), Wrapping(0x34b0bcb5),
-                        Wrapping(0x391c0cb3), Wrapping(0x4ed8aa4a), Wrapping(0x5b9cca4f), Wrapping(0x682e6ff3),
-                        Wrapping(0x748f82ee), Wrapping(0x78a5636f), Wrapping(0x84c87814), Wrapping(0x8cc70208),
-                        Wrapping(0x90befffa), Wrapping(0xa4506ceb), Wrapping(0xbef9a3f7), Wrapping(0xc67178f2) ];
+static K: [Wrapping<u32>; 64] = [
+    Wrapping(0x428a2f98),
+    Wrapping(0x71374491),
+    Wrapping(0xb5c0fbcf),
+    Wrapping(0xe9b5dba5),
+    Wrapping(0x3956c25b),
+    Wrapping(0x59f111f1),
+    Wrapping(0x923f82a4),
+    Wrapping(0xab1c5ed5),
+    Wrapping(0xd807aa98),
+    Wrapping(0x12835b01),
+    Wrapping(0x243185be),
+    Wrapping(0x550c7dc3),
+    Wrapping(0x72be5d74),
+    Wrapping(0x80deb1fe),
+    Wrapping(0x9bdc06a7),
+    Wrapping(0xc19bf174),
+    Wrapping(0xe49b69c1),
+    Wrapping(0xefbe4786),
+    Wrapping(0x0fc19dc6),
+    Wrapping(0x240ca1cc),
+    Wrapping(0x2de92c6f),
+    Wrapping(0x4a7484aa),
+    Wrapping(0x5cb0a9dc),
+    Wrapping(0x76f988da),
+    Wrapping(0x983e5152),
+    Wrapping(0xa831c66d),
+    Wrapping(0xb00327c8),
+    Wrapping(0xbf597fc7),
+    Wrapping(0xc6e00bf3),
+    Wrapping(0xd5a79147),
+    Wrapping(0x06ca6351),
+    Wrapping(0x14292967),
+    Wrapping(0x27b70a85),
+    Wrapping(0x2e1b2138),
+    Wrapping(0x4d2c6dfc),
+    Wrapping(0x53380d13),
+    Wrapping(0x650a7354),
+    Wrapping(0x766a0abb),
+    Wrapping(0x81c2c92e),
+    Wrapping(0x92722c85),
+    Wrapping(0xa2bfe8a1),
+    Wrapping(0xa81a664b),
+    Wrapping(0xc24b8b70),
+    Wrapping(0xc76c51a3),
+    Wrapping(0xd192e819),
+    Wrapping(0xd6990624),
+    Wrapping(0xf40e3585),
+    Wrapping(0x106aa070),
+    Wrapping(0x19a4c116),
+    Wrapping(0x1e376c08),
+    Wrapping(0x2748774c),
+    Wrapping(0x34b0bcb5),
+    Wrapping(0x391c0cb3),
+    Wrapping(0x4ed8aa4a),
+    Wrapping(0x5b9cca4f),
+    Wrapping(0x682e6ff3),
+    Wrapping(0x748f82ee),
+    Wrapping(0x78a5636f),
+    Wrapping(0x84c87814),
+    Wrapping(0x8cc70208),
+    Wrapping(0x90befffa),
+    Wrapping(0xa4506ceb),
+    Wrapping(0xbef9a3f7),
+    Wrapping(0xc67178f2),
+];
 
 #[derive(Debug)]
 pub struct Sha256 {
@@ -75,7 +125,7 @@ impl Sha256 {
         let mut h: Wrapping<u32>;
         let mut T1: Wrapping<u32>;
         let mut T2: Wrapping<u32>;
-        let mut W : [Wrapping<u32>; 64] = [Wrapping(0u32); 64];
+        let mut W: [Wrapping<u32>; 64] = [Wrapping(0u32); 64];
 
         [a, b, c, d, e, f, g, h] = self.H;
 
@@ -87,8 +137,8 @@ impl Sha256 {
                 W[t] = sigma1(W[t - 2]) + W[t - 7] + sigma0(W[t - 15]) + W[t - 16];
             }
 
-            T1 = h + Sigma1(e) + Ch(e,f,g)+ K[t]+ W[t];
-            T2 = Sigma0(a) + Maj (a, b, c);
+            T1 = h + Sigma1(e) + Ch(e, f, g) + K[t] + W[t];
+            T2 = Sigma0(a) + Maj(a, b, c);
             h = g;
             g = f;
             f = e;
@@ -109,7 +159,10 @@ impl Sha256 {
         self.H[7] += h;
     }
 
-    pub fn update<'a, T>(&mut self, data: T) where T: IntoIterator<Item = &'a u8> {
+    pub fn update<'a, T>(&mut self, data: T)
+    where
+        T: IntoIterator<Item = &'a u8>,
+    {
         for this_byte in data {
             self.M[self.current_block_length_bytes] = *this_byte;
             self.current_block_length_bytes += 1;
@@ -135,14 +188,17 @@ impl Sha256 {
         if self.remaining_bytes_in_block() < 8 {
             self.update(&temp_buffer[0..self.remaining_bytes_in_block()]);
         }
-        self.update(&temp_buffer[0..self.remaining_bytes_in_block() - total_data_processed_bits.len()]);
+        self.update(
+            &temp_buffer[0..self.remaining_bytes_in_block() - total_data_processed_bits.len()],
+        );
 
         self.update(&total_data_processed_bits);
 
         let mut return_value: [u8; 32] = [0; 32];
 
         for counter in 0..8 {
-            return_value[(counter * 4)..((counter * 4) + 4)].copy_from_slice(&self.H[counter].0.to_be_bytes());
+            return_value[(counter * 4)..((counter * 4) + 4)]
+                .copy_from_slice(&self.H[counter].0.to_be_bytes());
         }
 
         return_value
@@ -170,11 +226,11 @@ fn sigma1(x: Wrapping<u32>) -> Wrapping<u32> {
 }
 
 fn Sigma0(x: Wrapping<u32>) -> Wrapping<u32> {
-    Wrapping(rotr(x.0, 2) ^ rotr(x.0, 13) ^ rotr (x.0, 22))
+    Wrapping(rotr(x.0, 2) ^ rotr(x.0, 13) ^ rotr(x.0, 22))
 }
 
 fn Sigma1(x: Wrapping<u32>) -> Wrapping<u32> {
-    Wrapping(rotr(x.0, 6) ^ rotr(x.0, 11) ^ rotr (x.0, 25))
+    Wrapping(rotr(x.0, 6) ^ rotr(x.0, 11) ^ rotr(x.0, 25))
 }
 
 #[cfg(test)]
@@ -187,10 +243,11 @@ mod tests {
         let mut sha256 = Sha256::new();
         sha256.update("abc".as_bytes());
         let actual = sha256.do_final();
-        let expected: [u8; 32] = [  0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea,
-                                    0x41, 0x41, 0x40, 0xde, 0x5d, 0xae, 0x22, 0x23,
-                                    0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c,
-                                    0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad];
+        let expected: [u8; 32] = [
+            0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 0x41, 0x41, 0x40, 0xde, 0x5d, 0xae,
+            0x22, 0x23, 0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61,
+            0xf2, 0x00, 0x15, 0xad,
+        ];
         assert_eq!(actual, expected);
     }
 
@@ -200,10 +257,11 @@ mod tests {
         let mut sha256 = Sha256::new();
         sha256.update("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".as_bytes());
         let actual = sha256.do_final();
-        let expected: [u8; 32] = [  0x24, 0x8d, 0x6a, 0x61, 0xd2, 0x06, 0x38, 0xb8,
-                                    0xe5, 0xc0, 0x26, 0x93, 0x0c, 0x3e, 0x60, 0x39,
-                                    0xa3, 0x3c, 0xe4, 0x59, 0x64, 0xff, 0x21, 0x67,
-                                    0xf6, 0xec, 0xed, 0xd4, 0x19, 0xdb, 0x06, 0xc1];
+        let expected: [u8; 32] = [
+            0x24, 0x8d, 0x6a, 0x61, 0xd2, 0x06, 0x38, 0xb8, 0xe5, 0xc0, 0x26, 0x93, 0x0c, 0x3e,
+            0x60, 0x39, 0xa3, 0x3c, 0xe4, 0x59, 0x64, 0xff, 0x21, 0x67, 0xf6, 0xec, 0xed, 0xd4,
+            0x19, 0xdb, 0x06, 0xc1,
+        ];
         assert_eq!(actual, expected);
     }
 
@@ -216,10 +274,11 @@ mod tests {
             sha256.update(&data);
         }
         let actual = sha256.do_final();
-        let expected: [u8; 32] = [  0xcd, 0xc7, 0x6e, 0x5c, 0x99, 0x14, 0xfb, 0x92,
-                                    0x81, 0xa1, 0xc7, 0xe2, 0x84, 0xd7, 0x3e, 0x67,
-                                    0xf1, 0x80, 0x9a, 0x48, 0xa4, 0x97, 0x20, 0x0e,
-                                    0x04, 0x6d, 0x39, 0xcc, 0xc7, 0x11, 0x2c, 0xd0];
+        let expected: [u8; 32] = [
+            0xcd, 0xc7, 0x6e, 0x5c, 0x99, 0x14, 0xfb, 0x92, 0x81, 0xa1, 0xc7, 0xe2, 0x84, 0xd7,
+            0x3e, 0x67, 0xf1, 0x80, 0x9a, 0x48, 0xa4, 0x97, 0x20, 0x0e, 0x04, 0x6d, 0x39, 0xcc,
+            0xc7, 0x11, 0x2c, 0xd0,
+        ];
         assert_eq!(actual, expected);
     }
 }

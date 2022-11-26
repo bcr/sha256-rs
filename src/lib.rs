d3d296bdde9d19de377ada94f50b66ec62d58eb9
Blake Ramsdell <blaker@gmail.com>
2022-11-26 00:33:57 -0800

    Implemented remaining unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -199,4 +199,33 @@ mod tests {
                                     0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad];
         assert_eq!(actual, expected);
     }
+
+    // FIPS 180-2 §B.2, hashing the string "abcdbc..."
+    #[test]
+    fn test_b2() {
+        let mut sha256 = Sha256::new();
+        sha256.update("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".as_bytes());
+        let actual = sha256.do_final();
+        let expected: [u8; 32] = [  0x24, 0x8d, 0x6a, 0x61, 0xd2, 0x06, 0x38, 0xb8,
+                                    0xe5, 0xc0, 0x26, 0x93, 0x0c, 0x3e, 0x60, 0x39,
+                                    0xa3, 0x3c, 0xe4, 0x59, 0x64, 0xff, 0x21, 0x67,
+                                    0xf6, 0xec, 0xed, 0xd4, 0x19, 0xdb, 0x06, 0xc1];
+        assert_eq!(actual, expected);
+    }
+
+    // FIPS 180-2 §B.3, hashing the byte ’a’ 1,000,000 times (1,000 calls to update with 1,000 ’a’s apiece.)
+    #[test]
+    fn test_b3() {
+        let mut sha256 = Sha256::new();
+        let data: [u8; 1000] = [b'a'; 1000];
+        for _ in 0..1000 {
+            sha256.update(&data);
+        }
+        let actual = sha256.do_final();
+        let expected: [u8; 32] = [  0xcd, 0xc7, 0x6e, 0x5c, 0x99, 0x14, 0xfb, 0x92,
+                                    0x81, 0xa1, 0xc7, 0xe2, 0x84, 0xd7, 0x3e, 0x67,
+                                    0xf1, 0x80, 0x9a, 0x48, 0xa4, 0x97, 0x20, 0x0e,
+                                    0x04, 0x6d, 0x39, 0xcc, 0xc7, 0x11, 0x2c, 0xd0];
+        assert_eq!(actual, expected);
+    }
 }

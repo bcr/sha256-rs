3e67dc9cea442e9c84bdce0d6cf59b747480be36
Blake Ramsdell <blaker@gmail.com>
2022-11-28 18:05:06 -0800

    Reduce the use of Wrapping

--- src/lib.rs
+++ src/lib.rs
@@ -4,76 +4,20 @@ use std::io::{Read, Result};
 use std::mem;
 use std::num::Wrapping;
 
-static K: [Wrapping<u32>; 64] = [
-    Wrapping(0x428a2f98),
-    Wrapping(0x71374491),
-    Wrapping(0xb5c0fbcf),
-    Wrapping(0xe9b5dba5),
-    Wrapping(0x3956c25b),
-    Wrapping(0x59f111f1),
-    Wrapping(0x923f82a4),
-    Wrapping(0xab1c5ed5),
-    Wrapping(0xd807aa98),
-    Wrapping(0x12835b01),
-    Wrapping(0x243185be),
-    Wrapping(0x550c7dc3),
-    Wrapping(0x72be5d74),
-    Wrapping(0x80deb1fe),
-    Wrapping(0x9bdc06a7),
-    Wrapping(0xc19bf174),
-    Wrapping(0xe49b69c1),
-    Wrapping(0xefbe4786),
-    Wrapping(0x0fc19dc6),
-    Wrapping(0x240ca1cc),
-    Wrapping(0x2de92c6f),
-    Wrapping(0x4a7484aa),
-    Wrapping(0x5cb0a9dc),
-    Wrapping(0x76f988da),
-    Wrapping(0x983e5152),
-    Wrapping(0xa831c66d),
-    Wrapping(0xb00327c8),
-    Wrapping(0xbf597fc7),
-    Wrapping(0xc6e00bf3),
-    Wrapping(0xd5a79147),
-    Wrapping(0x06ca6351),
-    Wrapping(0x14292967),
-    Wrapping(0x27b70a85),
-    Wrapping(0x2e1b2138),
-    Wrapping(0x4d2c6dfc),
-    Wrapping(0x53380d13),
-    Wrapping(0x650a7354),
-    Wrapping(0x766a0abb),
-    Wrapping(0x81c2c92e),
-    Wrapping(0x92722c85),
-    Wrapping(0xa2bfe8a1),
-    Wrapping(0xa81a664b),
-    Wrapping(0xc24b8b70),
-    Wrapping(0xc76c51a3),
-    Wrapping(0xd192e819),
-    Wrapping(0xd6990624),
-    Wrapping(0xf40e3585),
-    Wrapping(0x106aa070),
-    Wrapping(0x19a4c116),
-    Wrapping(0x1e376c08),
-    Wrapping(0x2748774c),
-    Wrapping(0x34b0bcb5),
-    Wrapping(0x391c0cb3),
-    Wrapping(0x4ed8aa4a),
-    Wrapping(0x5b9cca4f),
-    Wrapping(0x682e6ff3),
-    Wrapping(0x748f82ee),
-    Wrapping(0x78a5636f),
-    Wrapping(0x84c87814),
-    Wrapping(0x8cc70208),
-    Wrapping(0x90befffa),
-    Wrapping(0xa4506ceb),
-    Wrapping(0xbef9a3f7),
-    Wrapping(0xc67178f2),
+static K: [u32; 64] = [
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 ];
 
 #[derive(Debug)]
 pub struct Sha256 {
-    H: [Wrapping<u32>; 8],
+    H: [u32; 8],
     M: [u8; 512 / 8],
     current_block_length_bytes: usize,
     total_data_processed_bytes: usize,
@@ -82,7 +26,7 @@ pub struct Sha256 {
 impl Sha256 {
     pub fn new() -> Sha256 {
         let mut return_value = Sha256 {
-            H: [Wrapping(0u32); 8],
+            H: [0u32; 8],
             M: [0; 512 / 8],
             current_block_length_bytes: 0,
             total_data_processed_bytes: 0,
@@ -102,62 +46,71 @@ impl Sha256 {
         // 180-2 §5.3.2. According to that section, “These words were obtained
         // by taking the first thirty-two bits of the fractional parts of the
         // square roots of the first eight prime numbers.”
-        self.H[0] = Wrapping(0x6a09e667);
-        self.H[1] = Wrapping(0xbb67ae85);
-        self.H[2] = Wrapping(0x3c6ef372);
-        self.H[3] = Wrapping(0xa54ff53a);
-        self.H[4] = Wrapping(0x510e527f);
-        self.H[5] = Wrapping(0x9b05688c);
-        self.H[6] = Wrapping(0x1f83d9ab);
-        self.H[7] = Wrapping(0x5be0cd19);
+        self.H[0] = 0x6a09e667;
+        self.H[1] = 0xbb67ae85;
+        self.H[2] = 0x3c6ef372;
+        self.H[3] = 0xa54ff53a;
+        self.H[4] = 0x510e527f;
+        self.H[5] = 0x9b05688c;
+        self.H[6] = 0x1f83d9ab;
+        self.H[7] = 0x5be0cd19;
 
         self.current_block_length_bytes = 0;
         self.total_data_processed_bytes = 0;
     }
 
     fn process_block(&mut self) {
-        let mut a: Wrapping<u32>;
-        let mut b: Wrapping<u32>;
-        let mut c: Wrapping<u32>;
-        let mut d: Wrapping<u32>;
-        let mut e: Wrapping<u32>;
-        let mut f: Wrapping<u32>;
-        let mut g: Wrapping<u32>;
-        let mut h: Wrapping<u32>;
-        let mut T1: Wrapping<u32>;
-        let mut T2: Wrapping<u32>;
-        let mut W: [Wrapping<u32>; 64] = [Wrapping(0u32); 64];
+        let mut a: u32;
+        let mut b: u32;
+        let mut c: u32;
+        let mut d: u32;
+        let mut e: u32;
+        let mut f: u32;
+        let mut g: u32;
+        let mut h: u32;
+        let mut T1: u32;
+        let mut T2: u32;
+        let mut W: [u32; 64] = [0u32; 64];
 
         [a, b, c, d, e, f, g, h] = self.H;
 
         for t in 0..64 {
             if t <= 15 {
                 let bytes: [u8; 4] = self.M[t * 4..(t * 4) + 4].try_into().unwrap();
-                W[t] = Wrapping(u32::from_be_bytes(bytes));
+                W[t] = u32::from_be_bytes(bytes);
             } else {
-                W[t] = sigma1(W[t - 2]) + W[t - 7] + sigma0(W[t - 15]) + W[t - 16];
+                W[t] = (Wrapping(sigma1(W[t - 2]))
+                    + Wrapping(W[t - 7])
+                    + Wrapping(sigma0(W[t - 15]))
+                    + Wrapping(W[t - 16]))
+                .0;
             }
 
-            T1 = h + Sigma1(e) + Ch(e, f, g) + K[t] + W[t];
-            T2 = Sigma0(a) + Maj(a, b, c);
+            T1 = (Wrapping(h)
+                + Wrapping(Sigma1(e))
+                + Wrapping(Ch(e, f, g))
+                + Wrapping(K[t])
+                + Wrapping(W[t]))
+            .0;
+            T2 = (Wrapping(Sigma0(a)) + Wrapping(Maj(a, b, c))).0;
             h = g;
             g = f;
             f = e;
-            e = d + T1;
+            e = (Wrapping(d) + Wrapping(T1)).0;
             d = c;
             c = b;
             b = a;
-            a = T1 + T2;
+            a = (Wrapping(T1) + Wrapping(T2)).0;
         }
 
-        self.H[0] += a;
-        self.H[1] += b;
-        self.H[2] += c;
-        self.H[3] += d;
-        self.H[4] += e;
-        self.H[5] += f;
-        self.H[6] += g;
-        self.H[7] += h;
+        self.H[0] = (Wrapping(self.H[0]) + Wrapping(a)).0;
+        self.H[1] = (Wrapping(self.H[1]) + Wrapping(b)).0;
+        self.H[2] = (Wrapping(self.H[2]) + Wrapping(c)).0;
+        self.H[3] = (Wrapping(self.H[3]) + Wrapping(d)).0;
+        self.H[4] = (Wrapping(self.H[4]) + Wrapping(e)).0;
+        self.H[5] = (Wrapping(self.H[5]) + Wrapping(f)).0;
+        self.H[6] = (Wrapping(self.H[6]) + Wrapping(g)).0;
+        self.H[7] = (Wrapping(self.H[7]) + Wrapping(h)).0;
     }
 
     pub fn update_from_reader(&mut self, reader: &mut dyn Read) -> Result<usize> {
@@ -226,18 +179,18 @@ impl Sha256 {
 
         for counter in 0..8 {
             return_value[(counter * 4)..((counter * 4) + 4)]
-                .copy_from_slice(&self.H[counter].0.to_be_bytes());
+                .copy_from_slice(&self.H[counter].to_be_bytes());
         }
 
         return_value
     }
 }
 
-fn Ch(x: Wrapping<u32>, y: Wrapping<u32>, z: Wrapping<u32>) -> Wrapping<u32> {
+fn Ch(x: u32, y: u32, z: u32) -> u32 {
     (x & y) ^ (!x & z)
 }
 
-fn Maj(x: Wrapping<u32>, y: Wrapping<u32>, z: Wrapping<u32>) -> Wrapping<u32> {
+fn Maj(x: u32, y: u32, z: u32) -> u32 {
     (x & y) ^ (x & z) ^ (y & z)
 }
 
@@ -245,20 +198,20 @@ fn rotr(x: u32, n: u8) -> u32 {
     (x >> n) | (x << (32 - n))
 }
 
-fn sigma0(x: Wrapping<u32>) -> Wrapping<u32> {
-    Wrapping(rotr(x.0, 7) ^ rotr(x.0, 18) ^ (x.0 >> 3))
+fn sigma0(x: u32) -> u32 {
+    rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3)
 }
 
-fn sigma1(x: Wrapping<u32>) -> Wrapping<u32> {
-    Wrapping(rotr(x.0, 17) ^ rotr(x.0, 19) ^ (x.0 >> 10))
+fn sigma1(x: u32) -> u32 {
+    rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10)
 }
 
-fn Sigma0(x: Wrapping<u32>) -> Wrapping<u32> {
-    Wrapping(rotr(x.0, 2) ^ rotr(x.0, 13) ^ rotr(x.0, 22))
+fn Sigma0(x: u32) -> u32 {
+    rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22)
 }
 
-fn Sigma1(x: Wrapping<u32>) -> Wrapping<u32> {
-    Wrapping(rotr(x.0, 6) ^ rotr(x.0, 11) ^ rotr(x.0, 25))
+fn Sigma1(x: u32) -> u32 {
+    rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25)
 }
 
 #[cfg(test)]
